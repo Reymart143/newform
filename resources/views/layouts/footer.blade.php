4583741006213c510fb1033dcb3351f49f28d10a
Lyzette Cagulada <79189652+Lyzette@users.noreply.github.com>
2023-03-06 22:47:25 +0800

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,11 @@
 <footer>
-    OBX Solutions Technology - Geo Pindot Interviews and TV Appearance, Video Promotions and other informations about the company.
+    <div class="footer">
+        <div class="row m-0 p-0 text-center">                    
+            <div class="col-md-4"><i class="fa-solid fa-location-dot"></i> VLC Tower One, Gran Via, Cagayan de Oro City, Misamis Oriental</div>
+            <div class="col-md-4"><i class="fa-regular fa-envelope"></i> info@obxsolution.com</div>
+            <div class="col-md-4"><i class="fa-solid fa-business-time"></i> 09:00 am - 05:00 pm</div>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
         <!-- Styles -->
         <style>
@@ -25,8 +26,10 @@
                 margin:0;
                 line-height:inherit;
             }
-            .container{
+            .container-fluid{
                 position: relative;
+                margin: 0;
+                padding: 0;
             }
             .login{   
                 margin-left: 88%;
@@ -37,15 +40,21 @@
                 top: 33px;
                 right: 150px;
             }
+            img.logoimg {
+                margin-left: 15px;
+                padding-bottom: 7px;
+            }
             .reg{
                 font-size: 18px;
                 color: antiquewhite;
                 position:absolute;
                 top: 22px;
                 right: 35px;
-                background-color: red;
+                background-color: #990f02;
                 padding: 3mm;  
                 font-weight: bold;
+                border-style: solid;
+                border-color: #990f02;
             }
             a:link { 
                 text-decoration: none; 
@@ -53,34 +62,69 @@
             a:visited { 
                 text-decoration: none; 
             }
-            a:hover { 
+            a:hover.login{ 
                 text-decoration: none; 
+                color: #990f02;
+                transition: 0.5s;
+            }
+            a:hover.reg{ 
+                text-decoration: none; 
+                background-color: #212121;
+                border-style: solid;
+                border-color: #990f02;
+                transition: 0.5s;
             }
             a:active { 
                 text-decoration: none;
             }
-            .container img{
+            .container-fluid img{
                 width: 40mm;
                 height: 20mm;
-
-            }
-            .body img{
-                width: 100%;
-                height: 100%;
             }
             .nav{
                 padding-top: 4mm;
                 padding-bottom: 2mm;
                 background-color: #212121;
             }
+            .footer{          
+                position: relative;
+                background-color: #990f02;
+                font-size: 14px;
+                color: white;
+                padding: 10px;
+            }
+            .body{
+                width: 100%;
+                height: 100vh;
+                background-image: url('images/bk2.jpg');
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: center;
+            }
         </style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
                    <!-- background-image:url({{url('images/bk.png')}}); -->
     <body class="antialiased">
-        <div class="container">
+
+        <!--footer-->
+        <footer>
+            <div class="footer">
+                <div class="row m-0 p-0 text-center">                    
+                    <div class="col-md-4"><i class="fa-solid fa-location-dot"></i> VLC Tower One, Gran Via, Cagayan de Oro City, Misamis Oriental</div>
+                    <div class="col-md-4"><i class="fa-regular fa-envelope"></i> info@obxsolution.com</div>
+                    <div class="col-md-4"><i class="fa-solid fa-business-time"></i> 09:00 am - 05:00 pm</div>
+                </div>
+            </div>
+        </footer>
+
+        <!--nav-->
+        <div class="container-fluid">
             <nav class="nav">
                 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                    <img src="images/logo2.png" alt="logo">
+                    <a href="/">
+                        <img src="images/logo2.png" alt="logo" class="logoimg">
+                    </a>
                     @if (Route::has('login'))
                         <div class="navbutt">
                             @auth
@@ -96,9 +140,13 @@
                     @endif
                 </div>
             </nav>
-            <div class="body">
-                <img src="images/bk.png" alt="logo">
+
+            <!--body-->
+            <div class="body">        
             </div>
         </div>
+
+        <!--bootstrap script-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
